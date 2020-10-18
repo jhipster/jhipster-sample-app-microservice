d67a76129f1a0a813cf5d9edda3dfd54cb599627
Pascal Grimaud <pascalgrimaud@gmail.com>
2020-10-18 22:24:17 +0200

    automatic project update

---
+++ src/main/java/io/github/jhipster/sample/config/Constants.java
@@ -0,0 +1,12 @@
+package io.github.jhipster.sample.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
---
+++ src/main/java/io/github/jhipster/sample/domain/BankAccount.java
@@ -0,0 +1,96 @@
+package io.github.jhipster.sample.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * A BankAccount.
+ */
+@Entity
+@Table(name = "bank_account")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class BankAccount implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "balance", precision = 21, scale = 2, nullable = false)
+    private BigDecimal balance;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BankAccount name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public BankAccount balance(BigDecimal balance) {
+        this.balance = balance;
+        return this;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof BankAccount)) {
+            return false;
+        }
+        return id != null && id.equals(((BankAccount) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", balance=" + getBalance() +
+            "}";
+    }
+}
