f344d2b89c98f03adba3fc96686d574ce3958073
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-06-18 15:26:19 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/config/WebConfigurer.java
+++ src/main/java/io/github/jhipster/sample/config/WebConfigurer.java
@@ -51,7 +51,7 @@ public class WebConfigurer implements ServletContextInitializer {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = jHipsterProperties.getCors();
-        if (!CollectionUtils.isEmpty(config.getAllowedOrigins())) {
+        if (!CollectionUtils.isEmpty(config.getAllowedOrigins()) || !CollectionUtils.isEmpty(config.getAllowedOriginPatterns())) {
             log.debug("Registering CORS filter");
             source.registerCorsConfiguration("/api/**", config);
             source.registerCorsConfiguration("/management/**", config);
